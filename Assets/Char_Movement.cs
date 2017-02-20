f68fd21549a2d0c6317a5b84ba3beee6d9a1217b
aguazul <aguazul@gmail.com>
2017-02-19 20:35:29 -0800

    Movement 1.0
    Movement is working!

--- Assets/Char_Movement.cs
+++ Assets/Char_Movement.cs
@@ -4,64 +4,109 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class Char_Movement : MonoBehaviour
 {
-
-    //Vr main camera
-    public Transform vrCamera;
-
-    //Angle which the walk stop will be triggered
-    public float toggleAngle = 30.0f;
-
-    //Stop / move
-    bool moveForward;
-
-    // Speed
-    float speed = 3.0f;
-
-    //acces the character controller
-    private CharacterController cc;
-
-
-
-   
+    // How fast to move
+    public float speed = 3.0F;
+    // Should I move forward or not
+    public bool moveForward, moveLeft, moveBack, moveRight, jump;
+    // CharacterController script
+    private CharacterController controller;
+    // GvrViewer Script
+    private GvrViewer gvrViewer;
+    // VR Head
+    private Transform vrHead;
 
     // Use this for initialization
     void Start()
     {
         // Find the CharacterController
-        cc= GetComponent<CharacterController>();
-
+        controller = GetComponent<CharacterController>();
+        // Find the GvrViewer on child 0
+        gvrViewer = transform.GetChild(0).GetComponent<GvrViewer>();
+        // Fnd the VR Head
+        vrHead = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        //Check if Vr Cameria is below 30 but not above 90 degrees
-
-        if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f)
-        {
-            moveForward = true;
-        }
-        else
-        {
-            moveForward = false;
-        }
-
+        // In the Google VR button press
         if (GvrController.IsTouching)
         {
-            moveForward = true;
+            Vector2 touchPos = GvrController.TouchPos;
+
+            if (touchPos.x > 0.66)
+            {
+                //if (touchPos.y <= 0.33) {
+                //moveRight = !moveRight;
+                //moveForward = !moveForward;
+                //} 
+                if (touchPos.y > 0.33 && touchPos.y <= 0.66)
+                {
+                    moveRight = !moveRight;
+                }
+                //else {
+                //    moveBack = !moveBack;
+                //    moveRight = !moveRight;
+                //}
+            }
+            else if (touchPos.x > 0.33 && touchPos.x < 0.66)
+            {
+                if (touchPos.y <= 0.33)
+                {
+                    moveForward = !moveForward;
+                }
+                else if (touchPos.y > 0.33 && touchPos.y <= 0.66)
+                {
+                    jump = !jump;
+                }
+                else {
+                    moveBack = !moveBack;
+                }
+            }
+            else {
+                //if (touchPos.y <= 0.33) {
+                //    moveLeft = !moveLeft;
+                //    moveForward = !moveForward;
+                //}
+                if (touchPos.y > 0.33 && touchPos.y <= 0.66)
+                {
+                    moveLeft = !moveLeft;
+                }
+                //else {
+                //    moveBack = !moveBack;
+                //    moveLeft = !moveLeft;
+                //}
+            }
+            // Check to see if I should move
+            if (moveForward)
+            {
+                // Find the forward direction
+                Vector3 forward = vrHead.TransformDirection(Vector3.forward);
+                // Tell CharacterController to move forward
+                controller.SimpleMove(forward * speed);
+
+            }
+            if (moveBack)
+            {
+                // Find the forward direction
+                Vector3 forward = vrHead.TransformDirection(Vector3.forward);
+                // Tell CharacterController to move forward
+                controller.SimpleMove(forward * speed * (-1));
+
+            }
+            if(moveRight)
+            {
+                Vector3 right = vrHead.TransformDirection(Vector3.right);
+                // Tell CharacterController to move forward
+                controller.SimpleMove(right * speed);
+            }
+            if (moveLeft)
+            {
+                Vector3 right = vrHead.TransformDirection(Vector3.right);
+                // Tell CharacterController to move forward
+                controller.SimpleMove(right * speed * (-1));
+            }
         }
-
-        if (moveForward)
-        {
-            //Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
-
-            Vector3 forward2 = GvrController.Orientation * Vector3.forward;
-
-            //cc.SimpleMove(forward * speed);
-
-            cc.SimpleMove(forward2 * speed);
-        }
-
-    }    
+    }
 }
